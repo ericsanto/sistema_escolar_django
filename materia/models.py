92af09f8481e261000f176d6d62c98f6a580f87a
Eric <ericjesus403@gmail.com>
2024-02-20 16:03:32 -0300

    feat: Criação do model Subject

--- materia/models.py
+++ materia/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from aluno.models import Student
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField('Nome: ',  max_length=255)
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    first_grade = models.DecimalField(
+        'Primeira Nota:', max_digits=4, decimal_places=2, default=None)
+    second_grade = models.DecimalField(
+        'Segunda Nota:',  max_digits=4, decimal_places=2, default=None)
