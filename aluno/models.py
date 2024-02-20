f4fd4e260999c37a265dd6289a93f7d5785f20b0
Eric <ericjesus403@gmail.com>
2024-02-20 16:24:43 -0300

    Removendo field subject

--- aluno/models.py
+++ aluno/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from materia.models import Subject
 
 
 class Student(models.Model):
     name = models.CharField('Nome', max_length=255)
-    subject = models.ManyToManyField(Subject)
