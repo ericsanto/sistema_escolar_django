efb7b6a257673bee41b65e8fe2fe67f6e5cd3279
Eric <ericjesus403@gmail.com>
2024-02-20 16:15:19 -0300

    Criando model Grade

--- nota/models.py
+++ nota/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from aluno.models import Student
+from materia.models import Subject
+
+
+class Grade(models.Model):
+    student = models.ForeignKey('Estudante', Student, on_delete=models.PROTECT)
+    subject = models.ForeignKey('Disciplina', Subject, on_delete=models.PROTECT)
