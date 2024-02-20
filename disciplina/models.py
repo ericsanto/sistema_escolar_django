7c2d45da8b5e3249e0967b55c085a7d7be6522ef
Éric Santos <146742498+ericsanto@users.noreply.github.com>
2024-02-20 15:55:10 -0300

    Delete disciplina directory

--- disciplina/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Subjects(models.Model):
-    name = models.CharField('Disciplina:', max_length=255)
-    grade_one = models.DecimalField(max_digits=5, decimal_places=2)
-    grade_two = models.DecimalField(max_digits=5, decimal_places=2)
-    frequency = models.BooleanField('Frequencia:', default=False)
