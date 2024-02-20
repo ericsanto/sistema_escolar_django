23e9963d2facdde6cac23b3a69b146bfa8a6770a
Eric <ericjesus403@gmail.com>
2024-02-20 16:05:41 -0300

    Removendo field student

--- materia/models.py
+++ materia/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from aluno.models import Student
 
 
 class Subject(models.Model):
     name = models.CharField('Nome: ',  max_length=255)
-    student = models.ForeignKey(Student, on_delete=models.PROTECT)
     first_grade = models.DecimalField(
         'Primeira Nota:', max_digits=4, decimal_places=2, default=None)
     second_grade = models.DecimalField(
