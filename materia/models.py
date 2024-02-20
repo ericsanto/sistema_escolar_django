89984135f3590e4a9c2da52fcdcf19505b619241
Eric <ericjesus403@gmail.com>
2024-02-20 16:26:25 -0300

    Adicionando field student

--- materia/models.py
+++ materia/models.py
@@ -3,7 +3,3 @@ from django.db import models
 
 class Subject(models.Model):
     name = models.CharField('Nome: ',  max_length=255)
-    first_grade = models.DecimalField(
-        'Primeira Nota:', max_digits=4, decimal_places=2, default=None)
-    second_grade = models.DecimalField(
-        'Segunda Nota:',  max_digits=4, decimal_places=2, default=None)
