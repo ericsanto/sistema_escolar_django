1371a58cbd326df59a9b5575b5f8a46ad3975863
Eric <ericjesus403@gmail.com>
2024-02-20 16:23:30 -0300

    Criando model Grade

--- nota/models.py
+++ nota/models.py
@@ -4,5 +4,9 @@ from materia.models import Subject
 
 
 class Grade(models.Model):
-    student = models.ForeignKey('Estudante', Student, on_delete=models.PROTECT)
-    subject = models.ForeignKey('Disciplina', Subject, on_delete=models.PROTECT)
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
+    first_grade = models.DecimalField(
+        'Primeira Nota', max_digits=4, decimal_places=2)
+    second_grade = models.DecimalField(
+        'Segunda Nota', max_digits=4, decimal_places=2)
