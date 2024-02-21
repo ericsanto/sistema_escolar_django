5cd8e3bb3d5b4422d7dbcb04335bcc5c7e13a0af
Eric <ericjesus403@gmail.com>
2024-02-21 08:34:41 -0300

    Adicionando novos fields no model Student: number_rg, cpf, addres, name_father, name_mather e registration

--- aluno/models.py
+++ aluno/models.py
@@ -1,5 +1,30 @@
 from django.db import models
+import re
+import random
+from datetime import date
 
 
 class Student(models.Model):
     name = models.CharField('Nome', max_length=255)
+    number_rg = models.CharField(
+        'Número do RG', max_length=9, blank=True, null=True)
+    cpf = models.CharField('CPF', max_length=11)
+    address = models.CharField('Endereço', max_length=255)
+    name_father = models.CharField(
+        'Nome do Pai', max_length=255, blank=True, null=True)
+    name_mather = models.CharField(
+        'Nome da Mãe', max_length=255, blank=True, null=True)
+    registration = models.CharField(
+        'Matrícula', max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.registration:
+            year = str(date.today().year)
+            num = list()
+            for i in range(6):
+                num.append(str(random.randint(0, 9)))
+            values = [year] + num
+            self.registration = ''.join(values)
