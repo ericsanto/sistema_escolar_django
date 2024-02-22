6d8838d0a37a4f33f56a5bdad61ade050e6523b6
Eric <ericjesus403@gmail.com>
2024-02-22 15:22:08 -0300

    Implementando view para listar todas as matérias a qual o aluno está matriculado

--- aluno/views.py
+++ aluno/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Student
 from nota.models import Grade
+from materia.models import Subject
 
 
 # class StudentTemplateView(TemplateView):
@@ -21,8 +22,17 @@ class StudentListView(ListView):
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context['grades'] = Grade.objects.filter(student__registration=self.request.user)
+        context['grades'] = Grade.objects.filter(
+            student__registration=self.request.user)
         return context
-    
 
 
+class StudentSubjectListView(ListView):
+    model = Student
+    template_name = 'list_subject.html'
+    context_object_name = 'subjects'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        subject = Subject.objects.filter(
+            student__registration=self.request.user)
+        return subject
