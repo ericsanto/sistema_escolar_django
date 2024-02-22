026168da063ce037552cced64b63863257fdf94f
Eric <ericjesus403@gmail.com>
2024-02-22 15:20:53 -0300

    Criando StudentAdmin personalizado

--- aluno/admin.py
+++ aluno/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Student
 
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'registration']
+
+
+admin.site.register(Student, StudentAdmin)
