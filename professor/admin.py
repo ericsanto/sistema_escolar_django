e25fb94854e1c51578de4b91d952b59f6fb55a47
Eric <ericjesus403@gmail.com>
2024-02-22 15:40:33 -0300

    Criando e Registrando admin personalizado

--- professor/admin.py
+++ professor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Teacher
 
-# Register your models here.
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'subject']
+
+admin.site.register(Teacher, TeacherAdmin)
