8349c432e9ef1174b22cc911373dcf1ab51e5953
Eric <ericjesus403@gmail.com>
2024-02-22 15:39:29 -0300

    Criando models

--- professor/models.py
+++ professor/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from materia.models import Subject
+import random
+from datetime import date
 
-# Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField('Nome', max_length=255)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    registre_job = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.registre_job:
+            year = str(date.today().year)
+            numbers = list()
+            for a in range(11):
+                numbers.append(str(random.randint(0, 9)))
+
+            values = [year] + numbers
+            self.registre_job = ''.join(values)
+        super().save(*args, **kwargs)
